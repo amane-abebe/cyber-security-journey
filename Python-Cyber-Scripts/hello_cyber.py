c6bd84ff599276a76a8afce541128f9c71f3700e
amane-abebe <emanuelabebeet@gmail.com>
2026-03-10 09:50:40 +0300

    Fix : Removed embedded git and added all Python scripts

---
+++ Python-Cyber-Scripts/hello_cyber.py
@@ -0,0 +1,8 @@
+import os
+import socket
+import getpass
+
+print("--- AMANE'S CYBER TOOL V1 ---")
+print("User:", getpass.getuser())
+print("PC Name:", socket.gethostname())
+print("Status:Back in action!")
